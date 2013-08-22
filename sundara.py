c4113c0be967fc0207f085e27665805b68f2f6ae
unknown <rshipp@MZ022-40.adit.mines.edu>
2013-08-22 11:53:23 -0600

    Added initial imports, defined main().

--- sundara.py
+++ sundara.py
@@ -3,4 +3,11 @@
    For a beautiful web.
 """
 
+from markdown import Markdown
+import argparse
 
+def main():
+    """Main function - run sundara as a script."""
+
+if __name__ == "__main__":
+    main()
