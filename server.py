783d811b357b5662157085e776930b6fbc9ea4be
george <rpubaddr0@gmail.com>
2013-08-31 17:22:51 -0600

    Added server.py.

---
+++ server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Sundara jāla
+   For a beautiful web.
+
+   server.py
+   Implements the functionality for the `sundara runserver` command.
+   A tiny little HTTP server, not suitable for use in production
+   environments.
+"""
+
+
