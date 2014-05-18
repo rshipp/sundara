01629e38f766e440650c40b5b7074f8cf3bab903
george <rpubaddr0@gmail.com>
2014-05-17 23:52:28 -0600

    Implemented basic generate functionality.

--- sundara/sundara.py
+++ sundara/sundara.py
@@ -1,12 +1,29 @@
 """Sundara jāla: for a beautiful web.
 """
 
+import os
+from pygit2 import Repository
+
+from sundara.jala import Jala
+
 class Sundara():
     def __init__(self, dir):
         self.dir = dir
+        self.generate_dir = os.path.join(dir, 'www')
 
     def get_files(self):
-        pass
+        repo = Repository(self.dir)
+        return [ f.path for f in repo.index if (f.path.endswith('.md')
+                and f.path.startswith('md/')) ]
 
     def generate(self):
-        pass
+        jala = Jala()
+        for file in self.get_files():
+            html = jala.convert(open(file).read())
+            newfilename = os.path.join(self.generate_dir, file[3:-3],
+                    'index.html')
+            dir = os.path.dirname(newfilename)
+            if dir != '' and not os.path.exists(dir):
+                os.makedirs(dir)
+            with open(newfilename, "w+") as newfile:
+                newfile.write(html)
