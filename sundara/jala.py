0692c2876816e1cf38e2e190e64b656f8dcd6be8
george <rpubaddr0@gmail.com>
2014-05-18 13:48:45 -0600

    Added Bootstrap/jQuery and meta info to Jala.

--- sundara/jala.py
+++ sundara/jala.py
@@ -18,9 +18,27 @@ class Jala():
     def convert(self, md):
         markdown = Markdown(output_format="html5")
         soup = BeautifulSoup(DOCTYPE + markdown.convert(md), "html5lib")
+
+        # Add meta information.
+        soup.head.append(BeautifulSoup().new_tag('title'))
+        soup.title.string = ""
+        soup.head.append(BeautifulSoup().new_tag('meta',
+            charset='UTF-8'))
+
+        # Link in Bootstrap and jQuery.
+        soup.head.append(BeautifulSoup().new_tag('link', rel='stylesheet',
+            href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'))
+        soup.body.append(BeautifulSoup().new_tag('script',
+            src='//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js'))
+        soup.body.append(BeautifulSoup().new_tag('script',
+            src='//code.jquery.com/jquery-1.11.0.min.js'))
+
+        # Wrap everything in the appropriate tags.
+
+
         html = soup.prettify().split('\n')
 
-        # Fix indentation for non-tag lines
+        # Fix indentation for non-tag lines.
         for index, line in enumerate(html):
             if index > 0 and re.match("\s*[^<\s]", line) and re.match("\s+[^<\s]", html[index - 1]):
                 # if line isn't a tag and previous line is indented and not a tag
