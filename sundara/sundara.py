3496b93dfb2c67fcc53cb3908a5e051c1b270d2a
george <rpubaddr0@gmail.com>
2014-05-18 00:06:02 -0600

    Special treatment for index.md, added beginning of init.

--- sundara/sundara.py
+++ sundara/sundara.py
@@ -9,21 +9,30 @@ from sundara.jala import Jala
 class Sundara():
     def __init__(self, dir):
         self.dir = dir
-        self.generate_dir = os.path.join(dir, 'www')
+        self.md_dir = os.path.join(self.dir, 'md')
+        self.generate_dir = os.path.join(self.dir, 'www')
 
     def get_files(self):
         repo = Repository(self.dir)
-        return [ f.path for f in repo.index if (f.path.endswith('.md')
-                and f.path.startswith('md/')) ]
+        return [ f.path[3:] for f in repo.index if (f.path.endswith('.md')
+                and f.path.startswith(self.md_dir + os.path.sep)) ]
 
     def generate(self):
         jala = Jala()
         for file in self.get_files():
             html = jala.convert(open(file).read())
-            newfilename = os.path.join(self.generate_dir, file[3:-3],
-                    'index.html')
-            dir = os.path.dirname(newfilename)
-            if dir != '' and not os.path.exists(dir):
-                os.makedirs(dir)
+            if file == os.path.join(self.md_dir, 'index.md'):
+                newfilename = os.path.join(self.generate_dir, 'index.html')
+            else:
+                newfilename = os.path.join(self.generate_dir, file[3:-3],
+                        'index.html')
+                dir = os.path.dirname(newfilename)
+                if dir != '' and not os.path.exists(dir):
+                    os.makedirs(dir)
             with open(newfilename, "w+") as newfile:
                 newfile.write(html)
+
+    def init(self):
+        pygit2.init_repository(self.dir)
+        os.makedirs(self.md_dir)
+        os.makedirs(self.generate_dir)
