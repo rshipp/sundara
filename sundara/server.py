019d8f632335b7333dbabece40300a8b48829b12
george <rpubaddr0@gmail.com>
2014-05-17 19:21:27 -0600

    Reorganized server, implemented main script, updated gitignore.

--- sundara/server.py
+++ sundara/server.py
@@ -1,14 +1,18 @@
 """Sundara jāla: for a beautiful web.
 
-Implements the functionality for the `sundara server` command.
-A tiny little HTTP server, not suitable for use in production
+Implements the functionality for the built-in development server.
+A tiny little HTTP file server, not suitable for use in production
 environments.
 """
 
-from http.server import HTTPServer
-from SundaraRequestHandler import SundaraRequestHandler
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib.parse import unquote
 
-class SundaraServer:
+from sundara import config
+
+
+class SundaraServer():
     def __init__(self, ip='127.0.0.1', port=8080):
         self.ip = ip
         self.port = port
@@ -24,3 +28,31 @@ class SundaraServer:
         except KeyboardInterrupt:
             print("Server shutting down...")
             httpd.shutdown()
+
+
+class SundaraRequestHandler(SimpleHTTPRequestHandler):
+    """Implements the functionality for the `sundara runserver` command.
+    A tiny little HTTP server, not suitable for use in production
+    environments.
+
+    This is an http.server Request Handler.
+    """
+
+    def translate_path(self, path):
+        """This function overwrites that in
+        http.server.SimpleHTTPRequestHandler and modifies it to
+        serve files from the Sundara `generate` path instead of the
+        cwd.
+        """
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        path = os.path.normpath(unquote(path))
+        words = path.split(os.sep)
+        words = filter(None, words)
+        path = config.getGeneratePath()
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        return path
