1fa59245f97305be4e3546fb38a7ccae79019a30
george <rpubaddr0@gmail.com>
2014-06-08 21:28:32 -0600

    Added some tests for SundaraServer.

--- tests/testserver.py
+++ tests/testserver.py
@@ -1,19 +1,65 @@
 import unittest
+from unittest.mock import Mock
 
 import os
 import shutil
 import tempfile
 import uuid
+from configparser import ConfigParser
+from http.server import HTTPServer
 
 from sundara import server
 from sundara import config
+from sundara import resources
 
 class TestServer(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.dir = tempfile.mktemp()
+        os.makedirs(self.dir)
+        self.config_file = os.path.join(self.dir, config.PROJECT_CONF)
+        # Make sure user config can't interfere with the tests.
+        config.USER_CONF = ''
+        # Mock getcwd.
+        self.old_getcwd = os.getcwd
+        os.getcwd = lambda: self.dir
 
     def tearDown(self):
+        shutil.rmtree(self.dir)
+        # Restore getcwd.
+        os.getcwd = self.old_getcwd
+
+    def test_can_use_config(self):
+        with open(self.config_file, "w+") as f:
+            f.write(resources.CONFIG)
+        c = config.Config(self.dir)
+        c.config.set('sundara', 'generate', self.dir)
+        c.config.write(open(self.config_file, 'w+'))
+        try:
+            s = server.SundaraServer(config=c)
+        except Exception:
+            self.fail()
+        self.assertEquals(s.ip, c.get('server', 'ip'))
+        self.assertEquals(s.port, int(c.get('server', 'port')))
+
+    def test_can_use_without_config(self):
+        self.assertFalse(os.path.exists(self.config_file))
+        os.makedirs(os.path.join(self.dir, 'www/'))
+        try:
+            s = server.SundaraServer()
+        except Exception:
+            self.fail()
+        self.assertEquals(s.ip, '127.0.0.1')
+        self.assertEquals(s.port, 8080)
+
+    def test_excepts_if_no_path(self):
+        self.assertRaises(IOError, server.SundaraServer)
+
+    def test_run_serves(self):
+        os.makedirs(os.path.join(self.dir, 'www/'))
+        #server.SundaraServer().run()
+
+    def test_run_excepts(self):
         pass
 
 
