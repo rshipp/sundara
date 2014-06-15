d4218d1dc0f7e77a78d9e2c986ff9b35223d825e
george <rpubaddr0@gmail.com>
2014-06-14 21:36:11 -0600

    Renamed dir and file in projects.

--- sundara/projects.py
+++ sundara/projects.py
@@ -10,8 +10,8 @@ from sundara import config
 from sundara.tools import config2kwargs
 
 class Project():
-    def __init__(self, dir):
-        self.dir = dir
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
         self.index = 'index'
         self.header = 'header'
         self.footer = 'footer'
@@ -25,29 +25,29 @@ class Project():
                 self.md_ext,  # Ignore files named '.md'.
         ]
 
-        conf_file = os.path.join(self.dir, config.PROJECT_CONF)
+        conf_file = os.path.join(self.project_dir, config.PROJECT_CONF)
         if os.path.exists(conf_file):
-            self.config = config.Config(self.dir)
+            self.config = config.Config(self.project_dir)
             self.md_dir = self.config.get('sundara', 'md')
-            self.generate_path = os.path.join(self.dir,
+            self.generate_path = os.path.join(self.project_dir,
                     self.config.get('sundara', 'generate'))
             self.css_path = self.config.get('sundara', 'css')
             self.js_path = self.config.get('sundara', 'js')
         else:
             self.md_dir = 'md/'
-            self.generate_path = os.path.join(self.dir, 'www/')
+            self.generate_path = os.path.join(self.project_dir, 'www/')
             self.css_path = 'css/'
             self.js_path = 'js/'
 
-        self.md_path = os.path.join(self.dir, self.md_dir)
+        self.md_path = os.path.join(self.project_dir, self.md_dir)
 
     def get_stylesheets(self):
-        repo = pygit2.Repository(self.dir)
+        repo = pygit2.Repository(self.project_dir)
         return [ f.path[len(self.css_path):] for f in repo.index if (
             f.path.endswith('.css') and f.path.startswith(self.css_path)) ]
 
     def get_javascript(self):
-        repo = pygit2.Repository(self.dir)
+        repo = pygit2.Repository(self.project_dir)
         return [ f.path[len(self.js_path):] for f in repo.index if (
             f.path.endswith('.js') and f.path.startswith(self.js_path)) ]
 
@@ -76,7 +76,7 @@ class Project():
             return str()
 
     def get_files(self):
-        repo = pygit2.Repository(self.dir)
+        repo = pygit2.Repository(self.project_dir)
         return [ f.path[len(self.md_dir):] for f in repo.index if (f.path.endswith(self.md_ext)
                 and f.path.startswith(self.md_dir)) ]
 
@@ -92,23 +92,23 @@ class Project():
 
         jala_args = config2kwargs(self.config.config)
         jala_args.update({
-            'dir': self.dir,
+            'dir': self.project_dir,
             'header': self.get_header(),
             'nav': self.get_nav(),
             'footer': self.get_footer(),
         })
         jala = Jala(**jala_args)
-        for file in self.get_files():
-            if file not in self.skip:
+        for filename in self.get_files():
+            if filename not in self.skip:
                 # Find the filename for the generated HTML, and convert.
-                if file == os.path.join(self.index + self.md_ext):
+                if filename == os.path.join(self.index + self.md_ext):
                     html = jala.convert(open(os.path.join(self.md_path,
-                        file)).read(), homepage=True)
+                        filename)).read(), homepage=True)
                     newfilename = os.path.join(self.generate_path, self.index + self.html_ext)
                 else:
-                    html = jala.convert(open(os.path.join(self.md_path, file)).read())
+                    html = jala.convert(open(os.path.join(self.md_path, filename)).read())
                     newfilename = os.path.join(self.generate_path,
-                            file[:-len(self.md_ext)], self.index + self.html_ext)
+                            filename[:-len(self.md_ext)], self.index + self.html_ext)
                     dir = os.path.dirname(newfilename)
                     if dir != '' and not os.path.exists(dir):
                         os.makedirs(dir)
@@ -118,15 +118,15 @@ class Project():
                     newfile.write(html)
 
     def init(self):
-        pygit2.init_repository(self.dir)
+        pygit2.init_repository(self.project_dir)
         for dir in [ self.md_path, self.generate_path ]:
             try:
                 os.makedirs(dir)
             except OSError:
                 # Dir exists, so we don't need to create it.
                 pass
-        self.config = config.Config(self.dir)
+        self.config = config.Config(self.project_dir)
         for file in resources.INIT_FILES:
-            if not os.path.exists(os.path.join(self.dir, file)):
-                with open(os.path.join(self.dir, file), "w+") as f:
+            if not os.path.exists(os.path.join(self.project_dir, file)):
+                with open(os.path.join(self.project_dir, file), "w+") as f:
                     f.write(resources.INIT_FILES[file])
